ba07d469651b185cc34ff0d79807ed986950696c
AndrewMalcher <andrew.malcher.r@gmail.com>
2025-08-05 17:50:33 -0400

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "./_components/ui/button";
 
 const Home = () => {
 	return (
 		<div>
 			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button className="mt-4">Click Me</Button>
+      
 		</div>
 	);
 };
