281f020fb1e60647d2435870f3f95c906e63d411
AndrewMalcher <andrew.malcher.r@gmail.com>
2025-10-27 15:15:56 -0400

    feat: add authetication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 const Home = async () => {
   const { userId } = await auth();
