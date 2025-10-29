1a7c918aa4561e7d907283565273cb24cf034b09
AndrewMalcher <andrew.malcher.r@gmail.com>
2025-10-28 21:46:11 -0400

    feat: start to make the transaction page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,11 +1,24 @@
 import { Button } from "../_components/ui/button";
 
-const TransactionsPage = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Transactions</h1>
-            <Button className="mt-4">Transactions</Button>
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Transactions</h1>
+      <Button className="mt-4">Transactions</Button>
+
+      {transactions.map((transaction) => (
+        <div key={transaction.id} className="border-b p-4">
+          <p className="font-semibold">{transaction.name}</p>
+          <p className="text-sm text-gray-500">
+            {transaction.type} - {transaction.category}
+          </p>
+          <p className="text-lg font-bold">{transaction.amount.toString()}</p>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 export default TransactionsPage;
